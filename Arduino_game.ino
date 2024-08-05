43417410c793c751c6a4bdd103a678ded6433c46
Yanal Abzak <131192823+abzakcd@users.noreply.github.com>
2024-08-05 23:59:39 +0300

    Update Arduino_game.ino

--- Arduino_game.ino
+++ Arduino_game.ino
@@ -6,18 +6,12 @@ void setup() {
   pinMode(PIN_SWITCH, INPUT_PULLUP);
   isServer = (digitalRead(PIN_SWITCH) == LOW) ? true : false;
   if (isServer)
-    // performance_setup();
         setupGame();
 
-  // else
-  //   game_setup();
+
 }
 
 void loop(){
     if (isServer)
-    // performance_loop();
         loopGame();
-
-  // else
-  //   game_loop();
 }
